eae70eafe84e5c15cb517a2d23db18a8b81d1b0c
srimchi <sriman.chilam@gmail.com>
2019-09-15 19:36:38 -0400

    Inital commit

---
+++ rungame/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    bool gameEnd = false;
+    public float restartDelay = 1f;  
+    public void GameOver()
+    {
+        if (gameEnd == false)
+        {
+            gameEnd = true;
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    }
+
+}
---
+++ rungame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f; 
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // add forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // move right 
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a")) 
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+}
